95e0dba877117ddc30deb7345a84b2a5a593bdfb
Nanako NakayoshiA <e205729@ie.u-ryukyu.ac.jp>
2020-12-24 03:37:11 +0900

    3nd commit

--- report5/src/jp/ac/uryukyu/ie/e205729/Main.java
+++ report5/src/jp/ac/uryukyu/ie/e205729/Main.java
@@ -1,16 +1,8 @@
 
 public class Main {
     public static void main (String[] args){
-        String str = null; 
-        
-        try{
-            System.out.println(str.length());
-
-        }catch(NullPointerException e){
-            System.out.println("例外　NullPointerExceptionが発生しました");
-            System.out.println(e.getMessage());
-
-        }
+        String str = "百二十三";
+        int value = Integer.parseInt(str);
     }
     
 }
